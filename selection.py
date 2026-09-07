706b4985386dd12b8b88fdd06c0eec7f9a3e3ae7
Raza <contact@raza.tips>
2026-09-07 15:54:50 +0200

    refactor: extract overlay into a separate file

--- selection.py
+++ selection.py
@@ -1,33 +1,9 @@
 import pyatspi
-import tkinter as tk
 import threading
 import os
+from overlay import create_overlay
 
 DEBUG = True 
-
-root = tk.Tk()
-
-root.overrideredirect(True)
-root.attributes("-topmost", True)
-root.geometry("400x100+500+300")
-
-
-label = tk.Label(
-    root,
-    text="NOTHING",
-    font=("Arial", 20),
-    bg="white",
-    fg="black"
-)
-
-label.pack(fill="both", expand=True)
-
-
-
-def change_text(new_text):
-    label.config(text=new_text)
-
-    
 
 def on_text_caret_moved(event):
 
@@ -161,37 +137,16 @@ def start_pyatspi():
     pyatspi.Registry.start()
 
 
-def start_move(event):
-    root.x = event.x
-    root.y = event.y
-
-
-def move_window(event):
-
-    x = root.winfo_x() + event.x - root.x
-    y = root.winfo_y() + event.y - root.y
-
-    root.geometry(f"+{x}+{y}")
-
-
-label.bind("<Button-1>", start_move)
-label.bind("<B1-Motion>", move_window)
-
-
-
-root.bind(
-    "<Escape>",
-    lambda event: root.destroy()
-)
-
-
 
 pyatspi_thread = threading.Thread(
     target=start_pyatspi,
     daemon=True
 )
 
+root, change_text = create_overlay()
+
 pyatspi_thread.start()
 
-
 root.mainloop()
+
+
